27635e8d86b9c3f5a15c2a88af66b0d4c37b4686
Suyeong-Lim <tntn393@gmail.com>
2022-05-19 16:48:53 +0900

    add: router 추가

--- messenger/src/App.jsx
+++ messenger/src/App.jsx
@@ -1,5 +1,17 @@
+import MainPage from "../src/pages/MainPage";
+import LoginPage from "../src/pages/LoginPage";
+import { Route, Routes } from "react-router-dom";
+import { Path } from "../src/utills/constants";
+
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Routes>
+        <Route path={Path.Home} element={<LoginPage />} />
+        <Route path={Path.Chat} element={<MainPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- messenger/src/utills/constants.js
+++ messenger/src/utills/constants.js
@@ -1,1 +1,6 @@
 export const DEFAULT_PROFILE_IMG = "";
+
+export const Path = {
+  Home: "/",
+  Chat: "/Chat",
+};
