d8e8969479802c2f08bb5c466a8765f106e97fdc
Suyeong-Lim <tntn393@gmail.com>
2022-05-19 16:26:40 +0900

    add: user State 세팅

---
+++ messenger/src/stores/user.js
@@ -0,0 +1,18 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+const initialState = {
+  userId: 0,
+  userName: "",
+  profileImg: "",
+};
+
+export const userSlice = createSlice({
+  name: "userReducer",
+  initialState,
+  reducers: {
+    setUser: (_, action) => action.payload,
+  },
+});
+
+export const { setUser } = userSlice.actions;
+export default userSlice;
